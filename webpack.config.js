3e5dca613ba0cd0bf9d395b3fbac78863f2de6b8
Acostan13 <acostan@fau.edu>
2020-03-01 11:22:05 -0500

    loading images & injecting into html page

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const autoprefixer = require('autoprefixer')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -34,14 +35,25 @@ module.exports = {
                         }
                     },
                     {
-                        loader: 'postcss-loader', 
+                        loader: 'postcss-loader',
                         options: {
                             ident: 'postcss',
                             plugins: () => [autoprefixer()]
-                        } 
+                        }
                     }
                 ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'url_loader?limit=8000&name=images/[name].[ext]'
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + '/src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ]
 }
